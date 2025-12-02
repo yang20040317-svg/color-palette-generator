4ec87a102feb961c9b04295db16f3d6ca62c7f2c
yang20040317-svg <yang20040317@gmail.com>
2025-12-02 22:07:11 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // 明确指定根目录
   server: {
     port: 3001,
     open: true
+  },
+  // 明确指定入口文件
+  build: {
+    rollupOptions: {
+      input: './index.html'
+    }
   }
 })
